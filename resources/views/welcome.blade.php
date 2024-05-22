c971e24308bb1b5e849a122ab112ce7366922efa
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-22 08:17:55 +0200

    welcome blade

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends("layout")
+
+@section("title")
+    Welcome
+@endsection
+
+@section("pageContent")
+    <h1 class="text-center">Cooks for every occasion</h1>
+    <h2>Bring top gastronomy to your home</h2>
+    <p>Whether you are planning an intimate dinner for two,
+        a family celebration or a business event,
+        our professional chefs will provide you with an unforgettable culinary experience.
+        Enjoy sophisticated meals prepared in your kitchen, without stress or hassle.
+    </p>
+    <h2>Why choose us</h2>
+    <p>  <span class="fw-bold"> Top chefs: </span> "All our chefs are professionals with many years of experience in the best restaurants."</p>
+    <p> <span class="fw-bold"> Personalized service: </span>  "We adapt each dish to your wishes and needs."</p>
+    <p> <span class="fw-bold"> Quality ingredients: </span>  "We use only the highest quality and freshest ingredients to prepare your meals."</p>
+    <p> <span class="fw-bold"> Stress-free: </span> "Leave everything to us – from menu planning to cleaning up after the meal."</p>
+
+@endsection
+
+
