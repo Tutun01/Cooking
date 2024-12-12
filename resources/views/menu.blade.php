41674f9a84b2922c476e6a81a0516b2762c3979e
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-12-12 08:00:30 +0100

    * Add atctions (Edit and Delete product)

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -12,6 +12,7 @@
             <th scope="col">Name</th>
             <th scope="col">Type</th>
             <th scope="col">Description</th>
+            <th>Actions</th>
         </tr>
         </thead>
         <tbody>
@@ -21,6 +22,10 @@
                 <td>{{ $product->name }}</td>
                 <td>{{ $product->type }}</td>
                 <td>{{ $product->description }}</td>
+                <td>
+                    <a type="button" class="btn btn-primary">Edit</a>
+                    <a href="/admin/delete-product/{{$product->id}}" type="button" class="btn btn-danger">Delete</a>
+                </td>
             </tr>
                 @endforeach
         </tbody>
