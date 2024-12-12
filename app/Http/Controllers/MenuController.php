48fbacee9c311b8d36478aaee3b4576af6db454e
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-12-12 07:58:56 +0100

    * Create two functions: delete and add product

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -13,4 +13,30 @@ class MenuController extends Controller
         $allProducts = MenuModel::all();
         return view("menu", compact('allProducts'));
     }
+
+    public function delete($product)
+    {
+        $singleProduct =MenuModel::where(['id' => $product])->first();
+
+        $singleProduct->delete();
+        return redirect()->back();
+    }
+
+    public function addProduct(Request $request)
+    {
+        $request->validate([
+            "name" => "required|string|unique:menu,name",
+            "type" => "required|string",
+            "description" => "required|string|min:3",
+        ]);
+
+
+        MenuModel::create([
+            "name" => $request->get("name"),
+            "type" => $request->get("type"),
+            "description" => $request->get("description"),
+        ]);
+
+        return redirect()->route("menu");
+    }
 }
