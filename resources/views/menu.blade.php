4e7f536af4e58d8487bedb65325e65946f9c259e
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-02-02 13:13:19 +0100

    * Creating a new database and writing test data
    * Deleting content from all pages

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,29 +5,5 @@
 @endsection
 
 @section("pageContent")
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Name</th>
-            <th scope="col">Type</th>
-            <th scope="col">Description</th>
-            <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($allProducts as $product)
-            <tr>
-                <td>{{ $product->id }}</td>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->type }}</td>
-                <td>{{ $product->description }}</td>
-                <td>
-                    <a type="button" class="btn btn-primary">Edit</a>
-                    <a href="/admin/delete-product/{{$product->id}}" type="button" class="btn btn-danger">Delete</a>
-                </td>
-            </tr>
-                @endforeach
-        </tbody>
-    </table>
+
 @endsection
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -5,28 +5,8 @@
 @endsection
 
 @section("pageContent")
-    <table class="table">
-        <thead>
-        <tr>
-
-            <th class="table-success" scope="col">Type of cooking</th>
-            <th class="table-success" scope="col">Number of people</th>
-            <th class="table-success" scope="col">Price</th>
 
 
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($allType as $type)
-            <tr>
 
-                <td  >{{ $type->typeOfCooking }}</td>
-                <td  >{{ $type->numberOfPeople }}</td>
-                <td>{{ $type->price }}</td>
-
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
 @endsection
 
