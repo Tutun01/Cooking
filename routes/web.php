77c3e59db19db6faae01218bc1bf133d7e4d1045
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-02-04 14:20:36 +0100

    * Creating new routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\MenuItemController;
+use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/welcome', [HomeController::class, 'index']);
 
@@ -12,3 +16,30 @@ Route::get('/dashboard', function () {
 
 Route::get('/menu', [MenuController::class, 'displayMenu'])
             ->name('displayMenu');
+
+Route::get('/menuItem/{menu}', [MenuItemController::class, 'displayItem'])
+    ->name('menuItems');
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/login');
+})->name('logout');
+
+Route::get('/reservation', function () {
+    return view('reservation');
+})->middleware('auth');
+
+Route::get('/reservation', [ReservationController::class, 'create'])
+    ->middleware('auth');
+
+Route::post('/reservation', [ReservationController::class, 'store'])
+    ->middleware('auth');
