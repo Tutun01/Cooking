f7e78c3ff3e09cf2ce718243bdbaa91a109bf6c2
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-02-04 14:20:50 +0100

    * Creating js logic

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,33 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    if (document.querySelector('#requested_date')) {
+        initReservationCalendar();
+    }
+
+});
+
+function initReservationCalendar() {
+    flatpickr('#requested_date', {
+        dateFormat: 'Y-m-d',
+        minDate: 'today',
+        disable: window.reservedDates || [],
+        locale: {
+            firstDayOfWeek: 1
+        },
+        onDayCreate: function (dObj, dStr, fp, dayElem) {
+            const date = dayElem.dateObj.toISOString().split('T')[0];
+            if ((window.reservedDates || []).includes(date)) {
+                dayElem.style.background = '#ddd';
+                dayElem.style.color = '#999';
+            }
+        }
+    });
+
+    console.log('Reservation calendar initialized');
+}
